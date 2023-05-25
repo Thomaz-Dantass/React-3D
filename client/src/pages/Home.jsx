c2e9454def5a5d416ab4773f3e0fce103e2e297d
Thomaz <thomazpato@gmail.com>
2023-05-25 15:23:21 -0300

    adicionando o modelo 3D

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -43,7 +43,7 @@ const Home = () => {
 
                            <CustomButton 
                              type="filled"
-                             tittle="Personalize aqui"
+                             tittle="Customize aqui"
                              handleClick={()=> state.intro = false}
                              customStyles="w-fit px-4 py-2.5 font-bold text-sm"
                            />
