c50939f9805d31eba79d4085c426b1a89999d879
Thomaz <thomazpato@gmail.com>
2023-05-29 13:32:30 -0300

    adicionando servidor back-end

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,58 +1,58 @@
-import { motion,AnimatePresence } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { useSnapshot } from 'valtio';
 
 import state from '../store';
 import { CustomButton } from '../components';
-import { 
-    headContainerAnimation,
-    headContentAnimation,
-    headTextAnimation,
-    slideAnimation
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation
 } from '../config/motion';
 
 const Home = () => {
-    const snap = useSnapshot(state);
+  const snap = useSnapshot(state);
 
-    return(
-        <AnimatePresence>
-            {snap.intro && (
-               <motion.section className="home" {...slideAnimation('left')}>
-                   <motion.header {...slideAnimation("down")}>
-                       <img 
-                       src='./threejs.png'
-                       alt='logo'
-                       className="w-8 h-8 object-contain"
-                       />
-                   </motion.header>
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation('left')}>
+          <motion.header {...slideAnimation("down")}>
+            <img 
+              src='./threejs.png'
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
 
-                   <motion.div className="home-content" {...headContainerAnimation}>
-                       <motion.div {...headTextAnimation}>
-                           <h1 className="head-text">
-                            VAMOS <br className="xl:block hidden" /> NESSA.
-                           </h1>
-                       </motion.div>
-                       <motion.div
-                         {...headContentAnimation}
-                         className="flex flex-col gap-5"
-                       >
-                           <p className="max-w-md font-normal text-gray-600 text-base">
-                               Crie a sua camisa única e exclusiva com a nossa
-                               nova ferramenta de personalização 3D. <strong>Liberte a sua
-                               imaginação</strong>{" "} e defina seu próprio estilo.
-                           </p>
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                VAMOS <br className="xl:block hidden" /> COMEÇA.
+              </h1>
+            </motion.div>
+            <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+            >
+              <p className="max-w-md font-normal text-gray-600 text-base">
+              Crie a sua camisa única e exclusiva com a nossa
+              nova ferramenta de personalização 3D. <strong>Liberte a sua
+              imaginação</strong>{" "} e defina seu próprio estilo.
+              </p>
 
-                           <CustomButton 
-                             type="filled"
-                             tittle="Customize aqui"
-                             handleClick={()=> state.intro = false}
-                             customStyles="w-fit px-4 py-2.5 font-bold text-sm"
-                           />
-                       </motion.div>
-                   </motion.div>
-               </motion.section> 
-            )}
-        </AnimatePresence>
-    )
+              <CustomButton 
+                type="filled"
+                title="Personalize aqui"
+                handleClick={() => state.intro = false}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  )
 }
 
 export default Home
